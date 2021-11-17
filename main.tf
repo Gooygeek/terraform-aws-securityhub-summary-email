f4a40eb41b08739aa774e84c7330760a2bd6599b
Kieran Goldsworthy <kierang@kgol.xyz>
2021-11-17 15:34:41 +1100

    fix(lambda code): Path to file in module
    Previously it would try and get the file from the directory that TF is
    running from, this fix means it gets the file from the modules dir.
    This is done using a prefix to the file's path `${path.module}`.

--- main.tf
+++ main.tf
@@ -234,7 +234,7 @@ EOF
 }
 
 resource "aws_lambda_function" "sechub_summariser" {
-  filename      = "sec-hub-email.zip"
+  filename      = "${path.module}/sec-hub-email.zip"
   function_name = var.name
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.lambda_handler"
@@ -242,7 +242,7 @@ resource "aws_lambda_function" "sechub_summariser" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("sec-hub-email.zip")
+  source_code_hash = filebase64sha256("${path.module}/sec-hub-email.zip")
 
   runtime = "python3.7"
   timeout = 30
