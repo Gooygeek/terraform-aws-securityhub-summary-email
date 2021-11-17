d23f8a334ba8fb03707dbb805df5af2293385e97
Kieran Goldsworthy <kierang@kgol.xyz>
2021-11-17 16:22:26 +1100

    feat(λ): Dynamically zip file. Add email subject

--- main.tf
+++ main.tf
@@ -233,11 +233,17 @@ EOF
   managed_policy_arns = ["arn:${data.aws_partition.this.partition}:iam::aws:policy/AWSSecurityHubReadOnlyAccess"]
 }
 
+data "archive_file" "init" {
+  type        = "zip"
+  source_file = "${path.module}/sec-hub-email.py"
+  output_path = "${path.module}/sec-hub-email.zip"
+}
+
 resource "aws_lambda_function" "sechub_summariser" {
   filename      = "${path.module}/sec-hub-email.zip"
   function_name = var.name
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.lambda_handler"
+  handler       = "sec-hub-email.lambda_handler"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
